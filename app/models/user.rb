356b32874213147d12581fe0d9e28fd1aa957f61
peachy19 <prasg19@gmail.com>
2017-05-11 22:42:31 +0000

    Created login and register, now user is authenticated

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+
+  has_secure_password
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+end
