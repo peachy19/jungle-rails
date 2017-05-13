370bf733729524a549c87a7060b77279b4d57f67
peachy19 <prasg19@gmail.com>
2017-05-13 03:26:48 +0000

    Logged in user can delete the rating

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,25 @@
 class ReviewsController < ApplicationController
    # before_action :set_review
-
+before_filter :authorize
 def new
   @review = Review.new
 end
   def create
-    product = Product.find(params[:product_id])
+    @product = Product.find(params[:product_id])
     puts reviews_params
-    review = product.reviews.new(reviews_params)
+    review = @product.reviews.new(reviews_params)
     review.user = current_user
     if review.save
-      redirect_to product
+      redirect_to @product
 
     end
   # raise "Yay, I'm here!"
+  end
+
+  def destroy
+  @review = Review.find params[:id]
+    @review.destroy
+    redirect_to @review.product, notice: 'Review deleted!'
   end
 
 
